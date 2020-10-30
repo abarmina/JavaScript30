03d80c16119e4009a24542d2e27a26a78517f8fe
Anna Barmina <anutik-barmina@mail.ru>
2020-10-30 21:30:29 +0100

    Custom Video Player

---
+++ 11-videoPlayer/scripts.js
@@ -0,0 +1,55 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const ranges = player.querySelectorAll('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '▶' : '▌▌'
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += +(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    let percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function handleProgressUpdate(e) {
+    newTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = newTime;
+}
+
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', handleProgressUpdate);
+let mouseDown = false;
+progress.addEventListener('mousemove', (e) => mouseDown && handleProgressUpdate(e));
+progress.addEventListener('mousedown', () => mouseDown = true);
+progress.addEventListener('mouseup', () => mouseDown = false);
+
